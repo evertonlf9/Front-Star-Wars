937112a3d307f36c7de8bea0bd131f909ffc7a77
unknown <everton9dev@gmail.com>
2020-05-08 22:54:03 -0300

    Ajustes no arquivo do server.js

--- server.js
+++ server.js
@@ -4,11 +4,9 @@ const path = require('path');
 const app = express();
 
 app.use(express.static('./dist/front-star-wars-angular'));
-app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/*', (req, res) =>
-    // res.sendFile('index.html', {root: 'dist/front-star-wars-angular/'})
-    res.sendFile(path.resolve(__dirname, './dist/index.html'))
+    res.sendFile('index.html', {root: 'dist/Front-Star-Wars-Angular/'})
 );
 
 app.listen(process.env.PORT || 8080);
